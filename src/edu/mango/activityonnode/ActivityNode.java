538787abf208da71d0d46f7b7dcdec0f33ecaa25
JingJie Wu <jingjie6@hawaii.edu>
2017-11-03 23:06:28 -1000

    merge

--- src/edu/mango/activityonnode/ActivityNode.java
+++ src/edu/mango/activityonnode/ActivityNode.java
@@ -1,14 +1,19 @@
 package edu.mango.activityonnode;
 
-import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.HashSet;
+import java.util.Set;
 
 /**
- * The ActivityNode is a representation of one activity/task in an
- * activity-on-node network.
+ * The ActivityNode is a representation of one activity/task in an activity-on-node network.
  * @author Team Mango
  */
 public class ActivityNode {
+	
+	private static final AtomicInteger counter = new AtomicInteger();
+
+	/** Number that uniquely identifies the activity. */
+	private int id;
 
 	/** The name of the activity. */
 	private String name;
@@ -19,9 +24,24 @@ public class ActivityNode {
 	/** Whether the activity has been visited or not. */
 	private boolean visited;
 
+	/** The earliest time an activity can be started. */
+	private int earliestStartTime;
+
+	/** The latest time an activity can be started. */
+	private int latestStartTime;
+
+	/** The earliest time an activity can be finished. */
+	private int earliestFinishTime;
+
+	/** The latest time an activity can be finished. */
+	private int latestFinishTime;
+
+	/** The amount of time an activity can be delayed without delaying the whole project. */
+	private int slackTime;
+
 	/** 
-	 * The set of activities that depend on this activity. In other words, the
-	 * children cannot be completed unless this current activity is finished. 
+	 * The set of activities that depend on this activity. In other words, the children cannot be completed unless this
+	 * current activity is finished.
 	 */
 	Set<ActivityNode> children;
 
@@ -31,9 +51,14 @@ public class ActivityNode {
 	 * @param duration - the duration of the activity
 	 */
 	public ActivityNode(String name, int duration) {
+		this.id = counter.incrementAndGet();
 		this.name = name;
 		this.duration = duration;
 		this.visited = false;
+		this.earliestStartTime = 0;
+		this.latestStartTime = 0;
+		this.earliestFinishTime = 0;
+		this.latestFinishTime = 0;
 		this.children = new HashSet<>();
 	}
 
@@ -46,7 +71,7 @@ public class ActivityNode {
 	}
 
 	/**
-	 * Sets the duration of the activity
+	 * Sets the duration of the activity.
 	 * @param duration - the new duration of the activity
 	 */
 	public void setDuration(int duration) {
@@ -57,7 +82,7 @@ public class ActivityNode {
 	 * Gets the name of the activity.
 	 * @return the name of the activity
 	 */
-	public String getActivityName() {
+	public String getName() {
 		return name;
 	}
 
@@ -78,7 +103,7 @@ public class ActivityNode {
 	}
 
 	/**
-	 * Sets the children of the activity
+	 * Sets the children of the activity.
 	 * @param children - the new set of children for the activity
 	 */
 	public void setChildren(Set<ActivityNode> children) {
@@ -119,4 +144,91 @@ public class ActivityNode {
 		this.visited = visited;
 	}
 
+	/**
+	 * Gets the earliest start time of the activity.
+	 * @return the earliest start time of the activity
+	 */
+	public int getEarliestStartTime() {
+		return earliestStartTime;
+	}
+
+	/**
+	 * Sets the earliest start time of the activity.
+	 * @param duration - the new earliest start time of the activity
+	 */
+	public void setEarliestStartTime(int earliestStartTime) {
+		this.earliestStartTime = earliestStartTime;
+	}
+
+	/**
+	 * Gets the latest start time of the activity.
+	 * @return the latest start time of the activity
+	 */
+	public int getLatestStartTime() {
+		return latestStartTime;
+	}
+
+	/**
+	 * Sets the latest start time of the activity.
+	 * @param duration - the new latest start time of the activity
+	 */
+	public void setLatestStartTime(int latestStartTime) {
+		this.latestStartTime = latestStartTime;
+	}
+
+	/**
+	 * Gets the earliest finish time of the activity.
+	 * @return the earliest finish time of the activity
+	 */
+	public int getEarliestFinishTime() {
+		return earliestFinishTime;
+	}
+
+	/**
+	 * Sets the earliest finish time of the activity.
+	 * @param earliestFinishTime - the new earliest finish time of the activity
+	 */
+	public void setEarliestFinishTime(int earliestFinishTime) {
+		this.earliestFinishTime = earliestFinishTime;
+	}
+
+	/**
+	 * Gets the latest finish time of the activity.
+	 * @return the latest finish time of the activity
+	 */
+	public int getLatestFinishTime() {
+		return latestFinishTime;
+	}
+
+	/**
+	 * Sets the latest finish time of the activity.
+	 * @param duration - the new latest finish time of the activity
+	 */
+	public void setLatestFinishTime(int latestFinishTime) {
+		this.latestFinishTime = latestFinishTime;
+	}
+
+	/**
+	 * Gets the slack time of the activity.
+	 * @return the slack time of the activity
+	 */
+	public int getSlackTime() {
+		return slackTime;
+	}
+
+	/**
+	 * Sets the slack time of the activity.
+	 * @param slackTime - the new slack time of the activity
+	 */
+	public void setSlackTime(int slackTime) {
+		this.slackTime = slackTime;
+	}
+
+	/**
+	 * Gets the ID of the activity.
+	 * @return the ID of the activity
+	 */
+	public int getId() {
+		return id;
+	}
 }
--- src/edu/mango/activityonnode/Project.java
+++ src/edu/mango/activityonnode/Project.java
@@ -1,20 +1,26 @@
 package edu.mango.activityonnode;
 
-import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashSet;
-import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 
 /**
- * The Project is a representation of all activities that make up an
- * activity-on-node-network.
+ * The Project is a representation of all activities that make up an activity-on-node-network.
  * @author Team Mango
  */
 public class Project {
 
+	private static final AtomicInteger counter = new AtomicInteger();
+
+	/** Number that uniquely identifies the project. */
+	private int id;
+
 	/** The name of the project. */
 	private String name;
-
-//	private ActivityTable activityTable;
 
 	/** The set of activities that make up the project. */
 	private Set<ActivityNode> activities;
@@ -24,12 +30,18 @@ public class Project {
 	 * @param name - the name of the project
 	 */
 	public Project(String name) {
+		this.id = counter.incrementAndGet();
 		this.name = name;
-//		this.activityTable = new ActivityTable();
 		this.activities = new HashSet<>();
 	}
 
+	/**
+	 * Creates a new project.
+	 * @param name - the name of the project
+	 * @param activities - the activities in the project
+	 */
 	public Project(String name, Set<ActivityNode> activities) {
+		this.id = counter.incrementAndGet();
 		this.name = name;
 		this.activities = activities;
 	}
@@ -75,18 +87,25 @@ public class Project {
 	}
 
 	/**
+	 * Gets the ID of the project.
+	 * @return the ID of the project
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
 	 * Deletes an activity from the Project.
 	 * @param activity - the activity to delete
 	 */
 	public void deleteActivity(ActivityNode activity) {
-		// Go through the children of every node in the project and remove
-		// the activity to delete, if it exists
+		// Go through the children of every node in the project and remove the activity to delete, if it exists
 		for (ActivityNode act : activities) {
 			act.deleteChild(activity);
 		}
 		activities.remove(activity);
 	}
-	
+
 	/**
 	 * Marks every activity in the Project as unvisited.
 	 */
@@ -100,28 +119,24 @@ public class Project {
 	 * Gets a topological sorting of the activity-on-node network.
 	 * @return a list containing the activity-on-node, topologically sorted.
 	 */
-	public LinkedList<ActivityNode> getTopologicalSort() {
-		LinkedList<ActivityNode> sortedNetwork = new LinkedList<>();
+	public List<ActivityNode> getTopologicalSort() {
+		List<ActivityNode> sortedNetwork = new ArrayList<>();
 		markActivitiesAsUnvisited();
 		for (ActivityNode activity : activities) {
 			if (!activity.isVisited()) {
 				visitActivityNode(activity, sortedNetwork);
 			}
 		}
-//		for (ActivityNode node : sortedNetwork) {
-//			System.out.println(node.getActivityName());
-//		}
 		return sortedNetwork;
 	}
 
 	/**
-	 * Visits an activity node, and then recursively visits its child nodes if
-	 * it is undiscovered. After its child nodes are discovered, then the
-	 * activity is added to the head of a linked list.
+	 * Visits an activity node, and then recursively visits its child nodes if it is undiscovered. After its child
+	 * nodes are discovered, then the activity is added to the front of a list.
 	 * @param activity - the activity to visit
 	 * @param list - the sorted activity-on-node network
 	 */
-	private void visitActivityNode(ActivityNode activity, LinkedList<ActivityNode> list) {
+	private void visitActivityNode(ActivityNode activity, List<ActivityNode> list) {
 		activity.setVisited(true);
 		Set<ActivityNode> childNodes = activity.getChildren();
 		for (ActivityNode child : childNodes) {
@@ -129,32 +144,240 @@ public class Project {
 				visitActivityNode(child, list);
 			}
 		}
-		list.addFirst(activity);
+		list.add(0, activity);
 	}
 
-//	/**
-//	 * Adds an activity to the activity table and updates the AON network
-//	 * @param activityName of this activity
-//	 * @param duration of this activity
-//	 * @param predecessors activities required to complete before starting this activity
-//	 */
-//	public void addActivity(String activityName, int duration, List<Integer> predecessors) {
-//		activityTable.addActivity(activityName, predecessors, duration);
-//		start = activityTable.convertTableToAON();
-//	}
-//	
-//	/**
-//	 * Prints a graphical representation of this project in plain-text
-//	 */
-//	public void printProject() {
-//		//todo
-//	}
-//	
-//	/**
-//	 * Prints the activity table of this project in plain-text
-//	 */
-//	public void printTable() {
-//		activityTable.printTable();
-//	}
+	/**
+	 * Gets the critical path of the activity-on-node network. It requires three things: the earliest start times,
+	 * latest start times, and the slack time of every activity.
+	 * @return a list containing the critical path of the activity-on-node network
+	 */
+	public List<ActivityNode> getCriticalPath() {
+		resetTimes();
 
+		// Temporary nodes which serve as the single source and sink nodes for the entire network during traversal
+		ActivityNode start = new ActivityNode("Start", 0);
+		ActivityNode end = new ActivityNode("End", 0);
+		addStartActivity(start);
+		addEndActivity(end);
+
+		calculateEarliestStartTimes(start, end);
+		calculateLatestStartTimes(start, end);
+		// Recalculates the actual earliest/latest start and finish times and slack times
+		finalizeAllTimes();
+
+		List<ActivityNode> criticalPath = findCriticalPath(start, end);
+		removeEndActivity(end);
+		return criticalPath;
+	}
+
+	/**
+	 * Calculates the earliest start times of the activities in the network. The times calculated for each activity is
+	 * one less than their actual.
+	 * @param start - the start activity
+	 * @param end - the end activity
+	 */
+	private void calculateEarliestStartTimes(ActivityNode start, ActivityNode end) {
+		ArrayDeque<ActivityNode> queue = new ArrayDeque<>();
+		queue.offer(start);
+		while (!queue.isEmpty()) {
+			ActivityNode current = queue.poll();
+			List<ActivityNode> parents = getParentActivities(current);
+			int est = calculateEarliestStartTime(parents);
+			current.setEarliestStartTime(est);
+			Set<ActivityNode> children = current.getChildren();
+			queue.addAll(children);
+		}
+	}
+
+	/**
+	 * Calculates the latest start times of the activities in the network. The times calculated for each activity is
+	 * one less than their actual.
+	 * @param start - the start activity
+	 * @param end - the end activity
+	 */
+	private void calculateLatestStartTimes(ActivityNode start, ActivityNode end) {
+		ArrayDeque<ActivityNode> queue = new ArrayDeque<>();
+		end.setLatestStartTime(end.getEarliestStartTime());
+		queue.offer(end);
+		while (!queue.isEmpty()) {
+			ActivityNode current = queue.poll();
+			Set<ActivityNode> children = current.getChildren();
+			if (current != end) {
+				int lst = calculateLatestStartTime(current, children);
+				current.setLatestStartTime(lst);	
+			}
+			List<ActivityNode> parents = getParentActivities(current);
+			queue.addAll(parents);
+		}
+	}
+
+	/**
+	 * Finalizes the start, end, and slack times for the activities.
+	 */
+	private void finalizeAllTimes() {
+		for (ActivityNode activity : activities) {
+			int calculatedEst = activity.getEarliestStartTime();
+			int calculatedLst = activity.getLatestStartTime();
+			int duration = activity.getDuration();
+			activity.setEarliestStartTime(calculatedEst + 1);
+			activity.setEarliestFinishTime(calculatedEst + duration);
+			activity.setLatestStartTime(calculatedLst + 1);
+			activity.setLatestFinishTime(calculatedLst + duration);
+			activity.setSlackTime(calculatedLst - calculatedEst);
+		}
+	}
+
+	/**
+	 * Resets the earliest/latest start, finish, and slack times of every activity.
+	 */
+	private void resetTimes() {
+		for (ActivityNode activity : activities) {
+			activity.setEarliestStartTime(0);
+			activity.setEarliestFinishTime(0);
+			activity.setLatestStartTime(0);
+			activity.setLatestFinishTime(0);
+			activity.setSlackTime(0);
+		}
+	}
+
+	/**
+	 * Adds a start activity, whose purpose is to act as the temporary, single source node for the entire network.
+	 * @param start - the start activity
+	 */
+	private void addStartActivity(ActivityNode start) {
+		List<ActivityNode> startingActivities = getStartingActivities();
+		for (ActivityNode activity : startingActivities) {
+			start.addChild(activity);
+		}
+	}
+
+	/**
+	 * Adds an end activity, whose purpose is to act as the temporary, single sink node for the entire network.
+	 * @param end - the end activity
+	 */
+	private void addEndActivity(ActivityNode end) {
+		for (ActivityNode activity : activities) {
+			if (activity.getChildren().size() == 0) {
+				activity.getChildren().add(end);
+			}
+		}
+	}
+
+	/**
+	 * Gets the set of the starting activities.
+	 * @return the set of starting activities
+	 */
+	private List<ActivityNode> getStartingActivities() {
+		List<ActivityNode> startingActivities = new ArrayList<>();
+		for (ActivityNode activity : activities) {
+			List<ActivityNode> parents = getParentActivities(activity);
+			if (parents.isEmpty()) {
+				startingActivities.add(activity);
+			}
+		}
+		return startingActivities;
+	}
+
+	/**
+	 * Gets the parents of an activity.
+	 * @param activity - the activity to check
+	 * @return a list containing the parents of an activity
+	 */
+	private List<ActivityNode> getParentActivities(ActivityNode activity) {
+		List<ActivityNode> predecessors = new ArrayList<>();
+		for (ActivityNode a : activities) {
+			if (a.getChildren().contains(activity)) {
+				predecessors.add(a);
+			}
+		}
+		return predecessors;
+	}
+
+	/**
+	 * Calculates the earliest start time for an activity.
+	 * @param parents - the parents of an activity
+	 * @return the earliest start time of an activity
+	 */
+	private int calculateEarliestStartTime(List<ActivityNode> parents) {
+		int time = Integer.MIN_VALUE;
+		// Special case for the start node since it has no parent activities
+		if (parents.isEmpty()) {
+			return 0;
+		}
+		for (ActivityNode parent : parents) {
+			int possibleTime = parent.getEarliestStartTime() + parent.getDuration();
+			if (possibleTime > time) {
+				time = possibleTime;
+			}
+		}
+		return time;
+	}
+
+	/**
+	 * Calculates the latest start time for an activity.
+	 * @param current - the activity that is being looked at
+	 * @param children - the child activities of the current node
+	 * @return the latest start time of the current activity
+	 */
+	private int calculateLatestStartTime(ActivityNode current, Set<ActivityNode> children) {
+		int time = Integer.MAX_VALUE;
+		for (ActivityNode child : children) {
+			int possibleTime = child.getLatestStartTime() - current.getDuration();
+			if (possibleTime < time) {
+				time = possibleTime;
+			}
+		}
+		return time;
+	}
+
+	/**
+	 * Removes the end activity.
+	 * @param end - the end activity
+	 */
+	private void removeEndActivity(ActivityNode end) {
+		for (ActivityNode activity : activities) {
+			activity.deleteChild(end);
+		}
+	}
+
+	/**
+	 * Traverses through the network and finds the activities on the critical path.
+	 * @param start - the start node
+	 * @param end - the end node
+	 * @return a list containing the activities on the critical path
+	 */
+	private List<ActivityNode> findCriticalPath(ActivityNode start, ActivityNode end) {
+		List<ActivityNode> path = new ArrayList<>();
+		ArrayDeque<ActivityNode> queue = new ArrayDeque<>();
+		queue.add(start);
+		while (!queue.isEmpty()) {
+			ActivityNode current = queue.poll();
+			if (current.getSlackTime() == 0 &&
+					current != end &&
+					current != start &&
+					!path.contains(current)) {
+				path.add(current);
+			}
+			Set<ActivityNode> children = current.getChildren();
+			queue.addAll(children);
+		}
+		// Traversal through the network does not necessarily find the critical path in order, so sort the activities
+		path.sort(new Comparator<ActivityNode>() {
+			/**
+			 * Compares the earliest start time of two activities.
+			 * @param first - the first node to compare
+			 * @param second - the second node to compare
+			 * @return 1 if the first node's start time is greater, -1 is the second node's start time is greater, or 0
+			 * if both start times are equal.
+			 */
+			@Override
+			public int compare(ActivityNode first, ActivityNode second) {
+				if (first.getEarliestStartTime() > second.getEarliestStartTime()) return 1;
+				if (first.getEarliestStartTime() < second.getEarliestStartTime()) return -1;
+				return 0;
+			}
+		});
+		return path;
+	}
 }
