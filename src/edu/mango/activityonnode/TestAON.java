c6b92f8d10b52bc43b974da900244fae8991c389
r-robles <rfrobles4@gmail.com>
2017-11-12 22:57:36 -1000

    Add sample activity-on-node network

--- src/edu/mango/activityonnode/TestAON.java
+++ src/edu/mango/activityonnode/TestAON.java
@@ -1,29 +1,55 @@
 package edu.mango.activityonnode;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class TestAON {
-	public static void main(String args[]){
-		Project project1 = new Project();
+	public static void main(String[] args) {
+		Project project = new Project("Project");
+
+		// This is based on the activity-on-node network on
+		// https://en.wikipedia.org/wiki/Critical_path_method#Visualizing_critical_path_schedule
+		ActivityNode a = new ActivityNode("A", 10);
+		ActivityNode b = new ActivityNode("B", 20);
+		ActivityNode c = new ActivityNode("C", 5);
+		ActivityNode d = new ActivityNode("D", 10);
+		ActivityNode e = new ActivityNode("E", 20);
+		ActivityNode f = new ActivityNode("F", 15);
+		ActivityNode g = new ActivityNode("G", 5);
+		ActivityNode h = new ActivityNode("H", 15);
 		
-		List<Integer> list = new ArrayList<Integer>();
-		list.add(0);
+		a.addChild(f);
+		a.addChild(b);
+		a.addChild(h);
 		
-		List<Integer> list2 = new ArrayList<Integer>();
-		list2.add(0);
+		b.addChild(c);
+		c.addChild(g);
+		c.addChild(d);
+		d.addChild(e);
+		f.addChild(g);
+		g.addChild(e);
+		h.addChild(e);
+
+		project.addActivity(a);
+		project.addActivity(c);
+		project.addActivity(b);
+		project.addActivity(d);
+		project.addActivity(e);
+		project.addActivity(f);
+		project.addActivity(g);
+		project.addActivity(h);
 		
-		List<Integer> list3 = new ArrayList<Integer>();
-		list3.add(0);
-		list3.add(1);
-		
-		project1.addActivity("TEST1", 10, null);
-		project1.addActivity("TEST2", 10, list);
-		project1.addActivity("TEST3", 10, list2);
-		project1.addActivity("TEST4", 10, list3);
-		
-		project1.printTable();
-		
-		project1.printProject();
+		List<ActivityNode> critPath = project.getCriticalPath();
+		for (ActivityNode node : project.getActivities()) {
+			System.out.println(node.getName() + " " + node.getEarliestStartTime() + " " + node.getEarliestFinishTime());
+			System.out.println("  " + node.getLatestStartTime() + " " + node.getLatestFinishTime());
+			System.out.println(node.getSlackTime());
+		}
+
+		System.out.println("Critical Path");
+		for (ActivityNode node : critPath) {
+			System.out.println(node.getName() + " " + node.getEarliestStartTime() + " " + node.getEarliestFinishTime());
+			System.out.println("  " + node.getLatestStartTime() + " " + node.getLatestFinishTime());
+			System.out.println(node.getSlackTime());
+		}
 	}
 }
